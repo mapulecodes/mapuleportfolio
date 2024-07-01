2328313b5671fb4c45f88617316ae3b6ea8aa425
mapulecodes <mbuzwanamapule@gmail.com>
2024-07-01 09:45:11 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-   
-     </nav>
-  <router-view/>
+  <div class="wrapper">
+    <NavBar/>
+    <main class="container-fluid">
+      <router-view />
+    </main>
+  
+  </div>
 </template>
+<script>
+import NavBar from '@/components/NavBar.vue'
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+export default {
+  components: {
+    NavBar
 
-nav {
-  padding: 30px;
-}
+  
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
 }
+</script>
+<style src="@/assets/css/style.css">
 </style>
